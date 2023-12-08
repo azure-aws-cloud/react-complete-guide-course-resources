c1bc70ee5f1a5935f50f8232510382c17c8baf6e
Babban Magan <madfan2002@users.noreply.github.com>
2023-12-08 10:02:08 +0530

    use state - force the component to render

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,6 +1,4 @@
 import {useState} from "react";
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
 import {CORE_CONCEPTS} from "./data";
 
 import Header from './components/Header';
@@ -14,7 +12,7 @@ export const userData ={
 
 
 function App() {
-    const [selectedTopic,setSelectedTopic ]= useState('Please click a button state');
+    const [selectedTopic,setSelectedTopic ] = useState('Please click a button state');
     function handleSelect(selectedButton){
         //selectedButton values can be 'components', 'jsx','prop','state'
         setSelectedTopic(selectedButton);
@@ -26,18 +24,30 @@ function App() {
           <section id="core-concepts">
               <h2>Core Concepts</h2>
               <ul>
-                  <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+                  <CoreConcept title={CORE_CONCEPTS[0].title}
+                               description={CORE_CONCEPTS[0].description}
+                               image={CORE_CONCEPTS[0].image}/>
 
                   <CoreConcept {...CORE_CONCEPTS[1]}/>
 
-                  <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
-                  <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+                  <CoreConcept title={CORE_CONCEPTS[2].title}
+                               description={CORE_CONCEPTS[2].description}
+                               image={CORE_CONCEPTS[2].image}/>
+
+                  <CoreConcept title={CORE_CONCEPTS[3].title}
+                               description={CORE_CONCEPTS[3].description}
+                               image={CORE_CONCEPTS[3].image}/>
               </ul>
           </section>
           <section id="examples">
               <h2>Examples</h2>
               <menu>
-                  <TabButton label="Components" onSelect={()=>handleSelect('components')}></TabButton>
+                  <TabButton label="Components" onSelect={
+                      (event)=> {
+                          handleSelect('components');
+                          console.log(event);
+                      }
+                  }></TabButton>
                   <TabButton label="JSX" onSelect={()=>handleSelect('jsx')}></TabButton>
                   <TabButton label="Props" onSelect={()=>handleSelect('props')}></TabButton>
                   <TabButton label="State" onSelect={()=>handleSelect('state')}></TabButton>
--- code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
+++ code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,4 @@
 export default function TabButton({label,onSelect}){
-
-    function onClick(event){
-        alert('button click'+event.target.value);
-    }
-
     return (
         <button onClick={onSelect}>{label}</button>
     );
